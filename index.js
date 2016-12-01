b91ac2a9cfb319bd18d0488fa9f98d4366db7efc
XianAquino <xian_a@yahoo.com>
2016-12-01 11:05:50 -0600

    Added button

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 //this is a js file
 var runThis = ()=>{
-  for(var i = 0 ; i < 10000000; i++){
-    console.log("breaking");
+  for(var i = 0 ; i < 1000; i++){
+    $('body').append(`<button class = "alert">${i}</button>`);
+
   }
+  addEvent();
 }
 
 for (var i = 1; i > 0; i ++) {
 	console.log("Ethan Broke the App!")
 }
+
+var addEvent = function(){
+  $('.alert').on('click',function(ev){
+    ev.preventDefault();
+    alert($(this).text());
+
+  })
+}
+
+
+$('.click').on('click',function(event){
+  event.preventDefault();
+  runThis();
+})
