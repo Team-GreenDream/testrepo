1f72b1218947f35097af04ed1a86a096c9b0b6fd
XianAquino <xian_a@yahoo.com>
2016-12-01 11:05:50 -0600

    Added button

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 //this is a js file
 var runThis = ()=>{
-  for(var i = 0 ; i < 10000000; i++){
-    console.log("breaking");
+  for(var i = 0 ; i < 1000; i++){
+    $('body').append(`<button class = "alert">${i}</button>`);
+
   }
+  addEvent();
 }
+
+var addEvent = function(){
+  $('.alert').on('click',function(ev){
+    ev.preventDefault();
+    alert($(this).text());
+
+  })
+}
+
+
+$('.click').on('click',function(event){
+  event.preventDefault();
+  runThis();
+})
